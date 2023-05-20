cba99b3963ebdea61a5c1f8b8a3603d8094ec8bd
artsiomandryianauleo <artsiom.andryianau@leovegas.com>
2023-05-20 11:12:49 +0200

    Init commit

---
+++ src/main/java/com/andryianau/gptassistant/GptassistantApplication.java
@@ -0,0 +1,13 @@
+package com.andryianau.gptassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptassistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptassistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andryianau/gptassistant/GptassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andryianau.gptassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptassistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
